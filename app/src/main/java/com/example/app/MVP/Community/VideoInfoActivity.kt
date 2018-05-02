021a3c8229b1b2fcfd6f17514ada7f66f8d2007c
6onlyyou <1790535171@qq.com>
2018-05-02 09:30:34 +0800

    增加弹框

--- app/src/main/java/com/example/app/MVP/Community/VideoInfoActivity.kt
+++ app/src/main/java/com/example/app/MVP/Community/VideoInfoActivity.kt
@@ -3,12 +3,18 @@ package com.example.app.MVP.Community
 import android.view.MotionEvent
 import android.view.View
 import android.widget.EditText
+import com.example.app.MVP.Community.callback.DialogFragmentDataCallback
 import com.example.app.MVP.Community.fragment.CommentFragment
 import com.example.app.R
 import com.example.app.base.BaseActivity
+import com.fushuaige.common.utils.ToastUtils
 import kotlinx.android.synthetic.main.activity_video_info.*
 
-class VideoInfoActivity : BaseActivity() {
+class VideoInfoActivity : BaseActivity(), DialogFragmentDataCallback {
+    override fun addDanmakuToView(content: String?) {
+        ToastUtils.showLong("评论"+content);
+    }
+
     override fun loadViewLayout() {
         setContentView(R.layout.activity_video_info);
         initTitle("视频", R.color.colorPrimary, R.color.white)
