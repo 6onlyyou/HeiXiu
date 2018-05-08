f521a3f58c567c0428f46286d52a4b7b7bdb842d
yuanGang <592357390@qq.com>
2018-05-08 15:59:51 +0800

    补充

---
+++ app/src/main/java/com/heixiu/errand/MVP/Login/entity/MessageEvent.java
@@ -0,0 +1,16 @@
+package com.heixiu.errand.MVP.Login.entity;
+
+/**
+ * Description:
+ * Data：2018/5/8-10:22
+ * Author: fushuaige
+ */
+public class MessageEvent {
+    public String limit;
+    public String city;
+
+    public MessageEvent(String limit, String city) {
+        this.limit = limit;
+        this.city = city;
+    }
+}
