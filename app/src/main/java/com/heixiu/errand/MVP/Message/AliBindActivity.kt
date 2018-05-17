30ce1b174c06e3092866a569b481812fdb6ec8ed
6onlyyou <1790535171@qq.com>
2018-05-17 10:48:45 +0800

    支付宝绑定页

---
+++ app/src/main/java/com/heixiu/errand/MVP/Message/AliBindActivity.kt
@@ -0,0 +1,22 @@
+package com.heixiu.errand.MVP.Message
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.heixiu.errand.R
+import com.heixiu.errand.base.BaseActivity
+
+class AliBindActivity : BaseActivity() {
+    override fun loadViewLayout() {
+        setContentView(R.layout.activity_ali_bind)
+    }
+
+    override fun findViewById() {
+    }
+
+    override fun setListener() {
+    }
+
+    override fun processLogic() {
+    }
+
+}
