f2f2cbe26ac09c2c1f84e65b56f8b9d55e13885a
6onlyyou <1790535171@qq.com>
2018-05-25 13:51:50 +0800

    账户安全界面

---
+++ app/src/main/java/com/heixiu/errand/MVP/Seting/AccountActivity.kt
@@ -0,0 +1,22 @@
+package com.heixiu.errand.MVP.Seting
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.heixiu.errand.R
+import com.heixiu.errand.base.BaseActivity
+
+class AccountActivity : BaseActivity() {
+    override fun loadViewLayout() {
+        setContentView(R.layout.activity_account)
+    }
+
+    override fun findViewById() {
+    }
+
+    override fun setListener() {
+    }
+
+    override fun processLogic() {
+    }
+
+}
---
+++ app/src/main/java/com/heixiu/errand/MVP/Seting/SetingMainActivity.kt
@@ -0,0 +1,22 @@
+package com.heixiu.errand.MVP.Seting
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.heixiu.errand.R
+import com.heixiu.errand.base.BaseActivity
+
+class SetingMainActivity : BaseActivity() {
+    override fun loadViewLayout() {
+        setContentView(R.layout.activity_seting_main)
+    }
+
+    override fun findViewById() {
+    }
+
+    override fun setListener() {
+    }
+
+    override fun processLogic() {
+    }
+
+}
