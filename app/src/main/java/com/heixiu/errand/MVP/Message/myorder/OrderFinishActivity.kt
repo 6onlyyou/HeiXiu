e0a37f5466026ec18b3bf26fd5e2d78f03ca3ac8
yuanGang <592357390@qq.com>
2018-06-28 17:59:01 +0800

    我的发布

---
+++ app/src/main/java/com/heixiu/errand/MVP/Message/myorder/OrderFinishActivity.kt
@@ -0,0 +1,13 @@
+package com.heixiu.errand.MVP.Message.myorder
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.heixiu.errand.R
+
+class OrderFinishActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_finish)
+    }
+}
